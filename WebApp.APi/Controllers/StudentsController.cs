9e9f905ace885dfdb8601f9473378b77fc4801aa
@programmingst <lucky.a.acidera@gmail.com>
2025-06-19 00:35:50 +0800

    Models Entity

---
+++ WebApp.APi/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.APi.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] {"Ally", "Lucky", "Ysaki", "Aki", "Alliesum" };
+
+            return Ok(students);    
+        }
+
+    }
+
+}
